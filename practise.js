7dcfad400826478fc8c2d912117891e7a36db88a
sg2409644 <sg2409644@gmail.com>
2025-05-18 05:43:58 +0000

    practise_Set

---
+++ practise.js
@@ -0,0 +1,56 @@
+// Create a variable fullName and assign your full name. Log the length of your name.
+// let s="suman ghosh"
+// console.log(s.length);
+//Write a program to check the type of different values: true, 42, "hello", {}, [], null, undefined.
+// console.log(typeof true);
+// console.log(typeof {});//object
+// console.log(typeof []);//object
+// console.log(typeof null);//object
+// console.log(typeof undefined);//undefined
+
+//Reverse a string "JavaScript".
+// let s="javascript"
+// // s=s.split('').reverse().join('')
+// // console.log(s);
+// s=s.split('')
+// let c=0
+// for(let i=0;i<s.length;i++){
+//     if(s[i]==='a' || s[i]==='e' || s[i]==='i'|| s[i]==='o'||s[i]==='u'){
+//         c++
+//     }
+// }
+// console.log(c);
+
+
+// let s = "javascript";
+// let c = 0;
+
+// for (let i = 0; i < s.length; i++) {
+//     if ('aeiou'.includes(s[i])) {
+//         c++;
+//     }
+// }
+
+// console.log(c);
+
+// let s= "suman"
+// let p= s
+// s=s.split('').reverse().join('')
+// if(s===p){
+//     console.log("palindrome");
+// }
+// else{
+//     console.log("not palindrome");
+    
+// }
+
+// console.log(Math.round(Math.random()*100)+1);
+
+let arr=[12,52,8,5,54,47]
+arr.push(60)
+console.log(arr);
+
+
+
+
+
